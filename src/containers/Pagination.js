3405a5c290e459cfc88b97eef4f11489c9e0aa06
Jhu, Sin-Han <blueice0916@gmail.com>
2019-05-30 03:05:28 +0800

    - create component Pagination - redux store connect with Pagination container

--- src/containers/Pagination.js
+++ src/containers/Pagination.js
@@ -1,17 +1,53 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as ActionCenter from '../actions'
+import PaginationItem from '../components/Pagination'
 
-class pagination extends Component {
+class Pagination extends Component {
+  state = {
+    NowPage: 1,
+    TotalPage: 0,
+  }
+
+  handleChangePage = numberPage => {
+    const { fetchOtherPageRepository, queryString } = this.props
+    this.setState({ NowPage: numberPage })
+    fetchOtherPageRepository(numberPage, queryString)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const nowTotalPage = this.props.paginationCount
+    const nextTotalPage = nextProps.paginationCount
+    if (nowTotalPage !== nextTotalPage) {
+      this.setState({ TotalPage: nextTotalPage })
+      return false
+    }
+    return true
+  }
+
   render() {
-    return <Fragment>pagination</Fragment>
+    return (
+      <Fragment>
+        <PaginationItem
+          pageState={this.state}
+          handleClick={this.handleChangePage}
+        />
+      </Fragment>
+    )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  paginationCount: state.dataStore.PaginationCount,
+  queryString: state.dataStore.QueryString,
+})
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(ActionCenter, dispatch)
+}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(pagination)
+)(Pagination)
