95f5005cc76b8c13fc6f9fa7e71476adb8fd41d1
Jhu, Sin-Han <blueice0916@gmail.com>
2019-05-30 02:54:48 +0800

    add loading  page to Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,29 +3,34 @@ import { connect } from 'react-redux'
 import SearchBar from './SearchBar'
 import Pagination from './Pagination'
 import ResultTable from './ResultTable'
+import LoadingPage from '../components/LoadingPage'
 
 const styles = {
   Main: {
     boxSizing: 'border-box',
     border: '1px solid red',
-    minWidth: '100vw',
-    minHeight: '100vh',
+    width: '100vw',
+    height: '100vh',
   },
 }
 
 export class Main extends Component {
   render() {
+    const { loadingStatus } = this.props
     return (
       <div style={styles.Main}>
         <SearchBar />
         <ResultTable />
         <Pagination />
+        {loadingStatus && <LoadingPage />}
       </div>
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  loadingStatus: state.pageControl.Loading,
+})
 
 const mapDispatchToProps = {}
 
