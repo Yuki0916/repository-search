6e262b71061fbd0713d318a62506510879268402
Jhu, Sin-Han <blueice0916@gmail.com>
2019-05-31 01:39:43 +0800

    Updates

--- precache-manifest.0948a263199f516bcdd0db6cda048feb.js
+++ precache-manifest.0948a263199f516bcdd0db6cda048feb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "365a9cb4f43023739e1e92c5a4c6ee33",
+    "revision": "5f668095a48e1b94f7ae8839541bc78e",
     "url": "/repository-search/index.html"
   },
   {
-    "revision": "5379ee01121b80d264ad",
+    "revision": "d958beba4796f2024180",
     "url": "/repository-search/static/css/main.9db08b69.chunk.css"
   },
   {
-    "revision": "60cc7acfa876ca8e202e",
-    "url": "/repository-search/static/js/2.7d63e7fa.chunk.js"
+    "revision": "6f23e0d312d665c78ca8",
+    "url": "/repository-search/static/js/2.f2bb9fe9.chunk.js"
   },
   {
-    "revision": "5379ee01121b80d264ad",
-    "url": "/repository-search/static/js/main.abb175aa.chunk.js"
+    "revision": "d958beba4796f2024180",
+    "url": "/repository-search/static/js/main.22b43282.chunk.js"
   },
   {
     "revision": "25ea3454082c64426b2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/repository-search/precache-manifest.5f2196c60b856a998beeaea3c6ccc727.js"
+  "/repository-search/precache-manifest.0948a263199f516bcdd0db6cda048feb.js"
 );
 
 self.addEventListener('message', (event) => {
